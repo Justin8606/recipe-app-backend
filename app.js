aa0e61eb287dd6fbb826d0b05fec3e6fde66ea03
Justin8606 <justinshajuk@gmail.com>
2024-06-15 10:11:02 +0530

    data put into database but the password exposed,we have to do the hashing

--- app.js
+++ app.js
@@ -5,16 +5,21 @@ const cors = require("cors")
 const users = require("./models/user")
 const {userModel} = require("./models/user")
 
+
+
 const app = express()
 app.use(cors())
 
 app.use(express.json())
 
+mongoose.connect("mongodb+srv://justin:nitsuj21@cluster0.3jf2qw3.mongodb.net/recipedb?retryWrites=true&w=majority&appName=Cluster0")
+
 app.post("/signup",(req,res)=>{
     let input = req.body
     // console.log(input)
     let user = new userModel(input)
-    console.log(user)               //by this step object also come in terminal
+    // console.log(user)               //by this step object also come in terminal
+    user.save()
     res.json({"status":"success"})
 })
 
